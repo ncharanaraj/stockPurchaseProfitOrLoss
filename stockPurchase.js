89b69c7a88a42b41ca7c40a287d26839c7b105c8
Charanaraj N <goudacharanraj760@gmail.com>
2023-01-02 12:25:41 +0530

    refactor: optimized func

--- stockPurchase.js
+++ stockPurchase.js
@@ -4,17 +4,21 @@ const currentPrice = document.querySelector("#current-price");
 const tellMeBtn = document.querySelector("#tell-me-btn");
 const displayMessage = document.querySelector("#display-message");
 
+function showMessage(message){
+    displayMessage.innerHTML = message;
+}
+
 function calculateProfitAndLoss(initial, quantity, current) {
     if (initial > current) {
         let loss = (initial - current) * quantity;
         let lossPercentage = (loss / initial) * 100;
-        displayMessage.innerText = `Hey!, the loss is ${loss} and the percentage is ${lossPercentage}%`
+        showMessage(`Hey!, the loss is ${loss} and the percentage is ${lossPercentage}%`)
     } else if (current > initial) {
         let profit = (current - initial) * quantity;
         let profitPercentage = (profit / initial) * quantity;
-        displayMessage.innerText = `Hey!, the profit is ${profit} and the percentage is ${profitPercentage}%`
+        showMessage(`Hey!, the profit is ${profit} and the percentage is ${profitPercentage}%`)
     } else {
-       displayMessage.innerText = "No pain no gain and no gain no pain"
+       showMessage("No pain no gain and no gain no pain")
     }
 }
 
