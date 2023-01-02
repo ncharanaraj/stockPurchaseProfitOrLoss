d7fec8fa1b77c675a2e34e79daa7e49ee2d587dc
Charanaraj N <goudacharanraj760@gmail.com>
2023-01-02 10:34:58 +0530

    dom reference

---
+++ stockPurchase.js
@@ -0,0 +1,6 @@
+const initialPrice = document.querySelector("#initial-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const tellMeBtn = document.querySelector("#tell-me-btn");
+const displayMessage = document.querySelector("#display-message");
+
