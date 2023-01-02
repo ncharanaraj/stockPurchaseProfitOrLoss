4f52e8562ac6aeb169e1541b85e2f29415118d9d
Charanaraj N <goudacharanraj760@gmail.com>
2023-01-02 11:36:31 +0530

    func added

--- stockPurchase.js
+++ stockPurchase.js
@@ -4,3 +4,22 @@ const currentPrice = document.querySelector("#current-price");
 const tellMeBtn = document.querySelector("#tell-me-btn");
 const displayMessage = document.querySelector("#display-message");
 
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
+        let loss = (initial - current) * quantity;
+        let lossPercentage = (loss / initial) * 100;
+        displayMessage.innerText = `Hey!, the loss is ${loss} and the percentage is ${lossPercentage}%`
+    } else if (current > initial) {
+        let profit = (current - initial) * quantity;
+        let profitPercentage = (profit / initial) * quantity;
+        displayMessage.innerText = `Hey!, the profit is ${profit} and the percentage is ${profitPercentage}%`
+    } else {
+       displayMessage.innerText = "No pain no gain and no gain no pain"
+    }
+}
+
+function clickHandler(){
+    calculateProfitAndLoss(initialPrice.value, stocksQuantity.value, currentPrice.value)
+}
+
+tellMeBtn.addEventListener('click', clickHandler)
